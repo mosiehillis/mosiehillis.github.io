ea3ea2948de464b76be69f7d6cfb109a9dc2f0d7
Mosie <mosiehillis@gmail.com>
2022-03-16 18:53:39 -0600

    upload images

--- upload.sh
+++ upload.sh
@@ -1,9 +1,9 @@
 for i in ./images/raws/*
 do
   full_outfile=./images/fulls/"$(basename "$i")"
-  convert "$i" -resize 1024x768 "$full_outfile"
+  convert "$i" -resize 1024 "$full_outfile"
   thumb_outfile=./images/thumbs/"$(basename "$i")"
-  convert "$i" -resize 512x256 "$thumb_outfile"
+  convert "$i" -resize 512 "$thumb_outfile"
 done
 git add --all
 git commit -m "upload images"
