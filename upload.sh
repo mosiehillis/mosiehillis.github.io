43b04d2d8dcc7951d48ee817f1b05f3c7718e080
Steven Hillis <steven.hillis@deepgram.com>
2022-04-20 11:32:05 -0600

    upload images

--- upload.sh
+++ upload.sh
@@ -2,4 +2,4 @@ bundle exec jekyll build
 git add --all
 git commit -m "upload images"
 git push
-git push `git subtree split --prefix _site master`:gh-pages --force
+git push origin `git subtree split --prefix _site master`:gh-pages --force
